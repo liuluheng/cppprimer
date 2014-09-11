bf71c9030ea9902cc47090d203bf4236d632fece
liuluheng <872287087@qq.com>
2014-09-11 17:00:15 +0800

    14/fcnobj.cc

--- 14/add.cc
+++ 14/add.cc
@@ -35,19 +35,19 @@ using std::cin; using std::cout; using std::endl;
 
 int main() 
 {
-    Sales_item item1, item2;
-    cin >> item1 >> item2;   //read a pair of transactions
-    // print the sum of two Sales_items
-    cout << item1 + item2 << endl; 
+  Sales_item item1, item2;
+  cin >> item1 >> item2;   //read a pair of transactions
+  // print the sum of two Sales_items
+  cout << item1 + item2 << endl; 
 
-	Sales_data data1, data2;
-    read(read(cin, data1), data2);   //read a pair of transactions
-    print(cout, add(data1, data2));  // print the sum of two Sales_datas
- 	cout << std::endl; 
+  Sales_data data1, data2;
+  read(read(cin, data1), data2);   //read a pair of transactions
+  print(cout, add(data1, data2));  // print the sum of two Sales_datas
+  cout << std::endl; 
 
-	cin >> data1 >> data2; // use input operator to read Sales_datas
-    // use operators to print the sum of two Sales_datas
-    cout << data1 + data2 << endl;
+  cin >> data1 >> data2; // use input operator to read Sales_datas
+  // use operators to print the sum of two Sales_datas
+  cout << data1 + data2 << endl;
 
-    return 0;
+  return 0;
 }
--- 14/calc.cc
+++ 14/calc.cc
@@ -52,36 +52,36 @@ auto mod = [](int i, int j) { return i % j; };
 // C library names in the global namespace.  Future printings will
 // change the name of this calss to divide.
 struct divide {
-    int operator()(int denominator, int divisor) {
-        return denominator / divisor;
-    }
+  int operator()(int denominator, int divisor) {
+    return denominator / divisor;
+  }
 };
 
 int main()
 {
-	function<int(int, int)> f1 = add;      // function pointer
-	function<int(int, int)> f2 = divide(); // callable class type
-	function<int(int, int)> f3 = [](int i, int j) // lambda
-	                             { return i * j; };
-	cout << f1(4,2) << endl; // prints 6
-	cout << f2(4,2) << endl; // prints 2
-	cout << f3(4,2) << endl; // prints 8
+  function<int(int, int)> f1 = add;      // function pointer
+  function<int(int, int)> f2 = divide(); // callable class type
+  function<int(int, int)> f3 = [](int i, int j) // lambda
+  { return i * j; };
+  cout << f1(4,2) << endl; // prints 6
+  cout << f2(4,2) << endl; // prints 2
+  cout << f3(4,2) << endl; // prints 8
 
-	// table of callable objects corresponding to each binary operator
-	// all the callables must take two ints and return an int
-	// an element can be a function pointer, function object, or lambda
-	map<string, function<int(int, int)>> binops = {
-		{"+", add},                  // function pointer
-		{"-", std::minus<int>()},    // library function object
-		{"/",  divide()},            // user-defined function object
-		{"*", [](int i, int j) { return i * j; }}, // unnamed lambda
-		{"%", mod} };                // named lambda object
+  // table of callable objects corresponding to each binary operator
+  // all the callables must take two ints and return an int
+  // an element can be a function pointer, function object, or lambda
+  map<string, function<int(int, int)>> binops = {
+    {"+", add},                  // function pointer
+    {"-", std::minus<int>()},    // library function object
+    {"/",  divide()},            // user-defined function object
+    {"*", [](int i, int j) { return i * j; }}, // unnamed lambda
+    {"%", mod} };                // named lambda object
 
-	cout << binops["+"](10, 5) << endl; // calls add(10, 5)
-	cout << binops["-"](10, 5) << endl; // uses the call operator of the minus<int> object
-	cout << binops["/"](10, 5) << endl; // uses the call operator of the divide object
-	cout << binops["*"](10, 5) << endl; // calls the lambda function object
-	cout << binops["%"](10, 5) << endl; // calls the lambda function object
+  cout << binops["+"](10, 5) << endl; // calls add(10, 5)
+  cout << binops["-"](10, 5) << endl; // uses the call operator of the minus<int> object
+  cout << binops["/"](10, 5) << endl; // uses the call operator of the divide object
+  cout << binops["*"](10, 5) << endl; // calls the lambda function object
+  cout << binops["%"](10, 5) << endl; // calls the lambda function object
 
-	return 0;
+  return 0;
 }
